e3eced8cd2c2d245bc1c5383ab89a7f12c99423b
Giuliana Sullca <114102411+GiulianaSC@users.noreply.github.com>
2022-10-08 19:03:50 -0500

    BackEnd
    Parte1

---
+++ api-usuario/api-usuario/entitys/usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_usuario.entitys
+{
+    public class usuario
+    {
+        //Definimos la clave primaria 
+        [Key]
+        public int codigo { get; set; }
+        //definimos valores no nulos
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(maximumLength:100,ErrorMessage ="Se tiene que ingresar un nombre")]
+        public String nombre { get; set; }
+        [Required]
+        public bool estado { get; set; }
+    }
+}
